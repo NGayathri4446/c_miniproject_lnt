37dc6188934291525a26c0c99efee44d5d676a78
ravali <ravalisree999@gmail.com>
2021-04-16 11:26:36 +0530

    modified

--- 3_Implementation/src/isValidDate.c
+++ 3_Implementation/src/isValidDate.c
@@ -2,24 +2,16 @@
 
 int isValidDate(Date *validDate)
 {
-    int validate=1;
+    
     //check range of year,month and day
     if (validDate->yyyy > MAX_YR ||
             validDate->yyyy < MIN_YR)
-            {
-                validate=0;
-            }
-        return validate;
+        return 0;
+
     if (validDate->mm < 1 || validDate->mm > 12)
-    {
-        validate=0;
-    }
-        return validate ;
+        return 0 ;
     if (validDate->dd < 1 || validDate->dd > 31)
-    {
-        validate=0;
-    }
-        return validate;
+        return 0;
     //Handle feb days in leap year
     if (validDate->mm == 2)
     {
@@ -32,5 +24,5 @@ int isValidDate(Date *validDate)
     if (validDate->mm == 4 || validDate->mm == 6 ||
             validDate->mm == 9 || validDate->mm == 11)
         return (validDate->dd <= 30);
-    return validate;
+    return 1;
 }
